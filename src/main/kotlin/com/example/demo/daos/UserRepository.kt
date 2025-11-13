801af211926374a3825bee00fbdc95240164bfe0
JC Mac <justinching365@gmail.com>
2025-11-13 12:53:49 -0700

    adding more neo4j User table DAO contracts + minor updates to fields

--- src/main/kotlin/com/example/demo/daos/UserRepository.kt
+++ src/main/kotlin/com/example/demo/daos/UserRepository.kt
@@ -3,11 +3,13 @@ package com.example.demo.daos
 import com.example.demo.models.User
 import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.data.neo4j.repository.Neo4jRepository
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
 interface UserRepository : Neo4jRepository<User, Long> {
     fun findByUsername(username: String): User?
+    fun findByEmail(email: String): User?
 
     @Query(
         """
@@ -26,20 +28,41 @@ interface UserRepository : Neo4jRepository<User, Long> {
         LIMIT ${'$'}limit
     """,
     )
-    fun findConnectionByLikeString(
-        username: String,
-        searchTerm: String,
-        limit: Int,
+    fun findUsersByLikeStringWithFriendPriority(
+        @Param("username") username: String,
+        @Param("searchTerm") searchTerm: String,
+        @Param("limit") limit: Int,
     ): List<User>
 
     @Query(
         """
-        MATCH (a:User {id: ${'$'}userId}), (b:User {id: $${'$'}otherUserId})
+        MATCH (a:User {userId: ${ '$' }userId})-[:CONNECTED_WITH]-(b:User)
+        RETURN b
+    """,
+    )
+    fun findConnections(
+        @Param("userId") userId: Long,
+    ): List<User>
+
+    @Query(
+        """
+        MATCH (a:User {userId: ${'$'}userId}), (b:User {userId: $${'$'}otherUserId})
         MERGE (a)-[:CONNECTED_WITH]-(b)
     """,
     )
     fun addConnection(
-        userId: Long,
-        otherUserId: Long,
+        @Param("userId") userId: Long,
+        @Param("otherUserId") otherUserId: Long,
+    )
+
+    @Query(
+        """
+        MATCH (a:User {userId: ${ '$' }userId})-[r:CONNECTED_WITH]-(b:User {userId: ${ '$' }otherId})
+        DELETE r
+    """,
+    )
+    fun removeConnection(
+        @Param("userId") userId: Long,
+        @Param("otherId") otherId: Long,
     )
 }
--- src/main/kotlin/com/example/demo/models/User.kt
+++ src/main/kotlin/com/example/demo/models/User.kt
@@ -5,11 +5,12 @@ import org.springframework.data.neo4j.core.schema.GeneratedValue
 import org.springframework.data.neo4j.core.schema.Id
 import org.springframework.data.neo4j.core.schema.Node
 import org.springframework.data.neo4j.core.schema.Relationship
+import java.time.LocalDateTime
 
 @Node("User")
 data class User(
     @Id @GeneratedValue
-    val user_id: Long? = null,
+    val userId: Long? = null,
     val username: String,
     val firstName: String,
     val lastName: String,
@@ -17,14 +18,5 @@ data class User(
     val phoneNumber: String,
     @Relationship(type = CONNECTED_WITH, direction = Relationship.Direction.OUTGOING)
     val friends: MutableSet<User> = mutableSetOf(),
-) {
-    fun connectTo(other: User) {
-        this.friends.add(other)
-        other.friends.add(this)
-    }
-
-    fun disconnectFrom(other: User) {
-        this.friends.remove(other)
-        other.friends.add(this)
-    }
-}
+    val joinDate: LocalDateTime,
+)
